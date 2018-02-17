f757999a6d2f7cf0e8764916c6b7c20cfc657ba4
jonaa21 <jona.oldenstam@outlook.com>
2018-02-17 11:43:27 -0300

    Adjusted onCreateView.SignUpFragment

--- Application/app/src/main/java/sr/unasat/joeyd/SignUpFragment.java
+++ Application/app/src/main/java/sr/unasat/joeyd/SignUpFragment.java
@@ -30,8 +30,8 @@ public class SignUpFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.signup_layout, container, false);
         initViews();
+        return inflater.inflate(R.layout.signup_layout, container, false);
     }
 
     //initialize all View components

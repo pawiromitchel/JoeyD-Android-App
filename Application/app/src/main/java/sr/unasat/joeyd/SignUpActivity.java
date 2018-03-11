67697ca8fed42b0a7c9cef784463f98bfbd95491
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2018-03-11 11:28:44 -0300

    Signup functionalitiy done

--- Application/app/src/main/java/sr/unasat/joeyd/SignUpActivity.java
+++ Application/app/src/main/java/sr/unasat/joeyd/SignUpActivity.java
@@ -1,6 +1,9 @@
 package sr.unasat.joeyd;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import sr.unasat.joeyd.database.JoeydDAO;
 import sr.unasat.joeyd.entity.User;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -17,54 +21,42 @@ public class SignUpActivity extends AppCompatActivity {
             password, confirmPassowrd;
     private static Button signUpBtn_frag;
 
+    private SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        EditText firstName = (EditText) findViewById(R.id.firstName);
-        EditText lastName = (EditText) findViewById(R.id.lastName);
-        EditText mobileNumber = (EditText) findViewById(R.id.mobileNumber);
-        EditText userName = (EditText) findViewById(R.id.username);
-        EditText password = (EditText) findViewById(R.id.password);
-        EditText confirmPassowrd = (EditText) findViewById(R.id.confirmPassword);
-        Button signUpBtn_frag = (Button) findViewById(R.id.fragment_signUp);
-    }
-
-   /* private View initViews(){
-        EditText firstName = (EditText) view.findViewById(R.id.firstName);
-        lastName = (EditText) view.findViewById(R.id.lastName);
-        mobileNumber = (EditText) view.findViewById(R.id.mobileNumber);
-        userName = (EditText) view.findViewById(R.id.username);
-        password = (EditText) view.findViewById(R.id.password);
-        confirmPassowrd = (EditText) view.findViewById(R.id.confirmPassword);
-        signUpBtn_frag = (Button) view.findViewById(R.id.fragment_signUp);
-        return view;
-    }*/
-
-    //Check validation
-    private User checkValidation(User user){
-        user = new User();
-        String getFirstName = firstName.getText().toString();
-        String getLastName = lastName.getText().toString();
-        String getMobileNumber = mobileNumber.getText().toString();
-        String getUserName = userName.getText().toString();
-        String getPassword = password.getText().toString();
-        String getConfirmPassword = confirmPassowrd.getText().toString();
-
-        // Check if all strings are null or not
-        if (getFirstName.equals("") || getFirstName.length() == 0
-                || getLastName.equals("") || getLastName.length() == 0
-                || getMobileNumber.equals("") || getMobileNumber.length() == 0
-                || getUserName.equals("") || getUserName.length() == 0
-                || getPassword.equals("") || getPassword.length() == 0
-                || getConfirmPassword.equals("")
-                || getConfirmPassword.length() == 0);
-        return user;
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
+        mobileNumber = (EditText) findViewById(R.id.mobileNumber);
+        userName = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassowrd = (EditText) findViewById(R.id.confirmPassword);
+        signUpBtn_frag = (Button) findViewById(R.id.fragment_signUp);
     }
 
     public void signUp(View view) {
-        checkValidation(new User());
-        signUpBtn_frag.setOnClickListener((View.OnClickListener) this);
+        if(!firstName.getText().toString().isEmpty() || !lastName.getText().toString().isEmpty() || !mobileNumber.getText().toString().isEmpty() || !userName.getText().toString().isEmpty() || !password.getText().toString().isEmpty()){
+            if(password.getText().toString().equals(confirmPassowrd.getText().toString())){
+                SQLiteOpenHelper joeyDDatabaseHelper = new JoeydDAO(this);
+                db = joeyDDatabaseHelper.getReadableDatabase();
+
+                ContentValues newUserValues = new ContentValues();
+                newUserValues.put("first_name", firstName.getText().toString());
+                newUserValues.put("last_name", lastName.getText().toString());
+                newUserValues.put("mobile_number", mobileNumber.getText().toString());
+                newUserValues.put("username", userName.getText().toString());
+                newUserValues.put("password", password.getText().toString());
+                long newUser = db.insert("user", null, newUserValues);
+                db.close();
+            } else {
+                Toast.makeText(this, "Your password does not match", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+
         Intent intent = new Intent(SignUpActivity.this, LoginScreenActivity.class);
         Toast.makeText(this, "You have succesfully signed up", Toast.LENGTH_SHORT).show();
     }

cbbf433d505a132734641ebb884e7302d2984e25
diepje <rgsd.mario@gmail.com>
2018-03-11 16:03:20 -0300

    nav_drawer

--- Application/app/src/main/java/sr/unasat/joeyd/MainMenuActivity.java
+++ Application/app/src/main/java/sr/unasat/joeyd/MainMenuActivity.java
@@ -124,19 +124,15 @@ public class MainMenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        if (id == R.id.nav_myAccount) {
+//            Intent myAccount = new Intent(this, MyAccountActivity.class);
+//            startActivity(myAccount);
+        } else if (id == R.id.nav_myReceipts) {
+            Intent myReceipts = new Intent(this, MyReceiptsActivity.class);
+            startActivity(myReceipts);
+        } else if (id == R.id.nav_About) {
+//            Intent pendingOrder = new Intent(this, PendingOrderActivity.class);
+//            startActivity(pendingOrder);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
